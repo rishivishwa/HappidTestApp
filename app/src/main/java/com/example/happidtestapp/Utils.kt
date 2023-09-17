bee87f1ae7eeaf61d8cd1529d74aef510dfebba4
rishivishwa <rishish780@gmail.com>
2023-09-18 01:04:22 +0530

    All done

--- app/src/main/java/com/example/happidtestapp/Utils.kt
+++ app/src/main/java/com/example/happidtestapp/Utils.kt
@@ -6,18 +6,7 @@ import android.text.style.ForegroundColorSpan
 
 object Utils {
 
-     fun extractTwoDigitsFromStartAndEnd(input: String): String {
-        val length = input.length
 
-        return if (length >= 4) {
-            val startDigits = input.substring(0, 2)
-            val endDigits = input.substring(length - 2)
-            "$startDigits$endDigits"
-        } else {
-            // Handle the case where the input has less than four digits
-            "Input too short"
-        }
-    }
     fun changeTextColorInSentence(sentence: String, targetText: String,targetTwo: String, color: Int): SpannableString {
         val spannable = SpannableString(sentence)
         val startIndex = sentence.indexOf(targetText)
